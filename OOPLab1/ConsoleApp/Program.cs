ffef38bf0cc46af6e725eb8d05d40109110afb6d
Dekron <forpresentelite@gmail.com>
2024-09-05 19:09:43 +0300

    Calculation of the sum of numbers and output of the result have been added

--- OOPLab1/ConsoleApp/Program.cs
+++ OOPLab1/ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("Hello world!");
-Console.ReadKey();
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int x = 5;
+        int y = 10;
+        Console.WriteLine("5 + 10 = {0}", x + y);
+        Console.ReadKey();
+    }
+}
