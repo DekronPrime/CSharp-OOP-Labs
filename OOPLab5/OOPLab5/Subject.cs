3a14367cd20a90d2e3dce56a38970f0b06af6815
Dekron <forpresentelite@gmail.com>
2024-11-14 03:12:39 +0200

    lab5 done

---
+++ OOPLab5/OOPLab5/Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLab5
+{
+    public enum FinalControlType
+    {
+        Exam,
+        Test,
+        None
+    }
+
+    public struct Subject
+    {
+        public string? Name { get; set; }
+        public string? Lecturer { get; set; }
+        public string? GroupName { get; set; }
+        public int StudentsCount { get; set; }
+        public FinalControlType FinalControl { get; set; }
+        public bool HasCourseWork { get; set; }
+        public string? Specialty { get; set; }
+        public int Semester { get; set; }
+
+        public override string ToString()
+        {
+            return $"Дисципліна: {Name},\nВикладач: {Lecturer},\nГрупа: {GroupName},\n" +
+                   $"Кількість студентів: {StudentsCount},\nТип контролю: {FinalControl},\n" +
+                   $"Курсова робота: {(HasCourseWork ? "є" : "немає")},\nСпеціальність: {Specialty},\n" +
+                   $"Семестр: {Semester}";
+        }
+    }
+}
