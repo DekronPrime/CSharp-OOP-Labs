ef7aa5b0b718130e57381147d7c67c87dd84df51
Dekron <forpresentelite@gmail.com>
2024-12-10 01:10:57 +0200

    lab10 done

---
+++ OOPLab10/Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+
+            studentDictionary.Add(1, new Student("Тарнавський П.В.", "КН-321", 3, 3000));
+
+            studentDictionary.Add(2, new Student("Коваленко О.М.", "КН-221", 2, 2500));
+
+            Console.WriteLine("Поточний список студентів у колекції:");
+            PrintDictionary(studentDictionary);
+
+            Console.WriteLine("\nВилучення студента з id = 1:");
+            studentDictionary.Remove(1);
+            Console.WriteLine("Поточний список студентів:");
+            PrintDictionary(studentDictionary);
+
+            Console.WriteLine("\nПеревірка наявності студента з id = 2:");
+            if (studentDictionary.ContainsKey(2))
+                Console.WriteLine($"Знайдено: {studentDictionary[2].FullName}");
+            else
+                Console.WriteLine("Студента не знайдено.");
+
+            Console.WriteLine($"\nКількість студентів у колекції: {studentDictionary.Count}");
+
+            Console.WriteLine("\nОчищення колекції студентів...");
+            studentDictionary.Clear();
+            Console.WriteLine($"Кількість студентів після очищення: {studentDictionary.Count}");
+        }
+
+        static void PrintDictionary(Dictionary<int, Student> dictionary)
+        {
+            foreach (var entry in dictionary)
+            {
+                Console.WriteLine($"Id: {entry.Key}, {entry.Value.FullName}, Група: {entry.Value.Group}, Курс: {entry.Value.CourseNumber}");
+            }
+        }
+
+    }
+}
---
+++ OOPLab10/Task2/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Result
+    {
+        public string SubjectName { get; set; }
+        public string TeacherFullName { get; set; }
+        public bool IsExam { get; set; }
+        public int Points { get; set; }
+
+        public Result(string subjectName, string teacherFullName, bool isExam, int points)
+        {
+            SubjectName = subjectName;
+            TeacherFullName = teacherFullName;
+            IsExam = isExam;
+            Points = points;
+        }
+
+        public override string ToString()
+        {
+            return $"Предмет: {SubjectName}, Викладач: {TeacherFullName}, Екзамен: {IsExam}, Оцінка: {Points}/100";
+        }
+    }
+}
